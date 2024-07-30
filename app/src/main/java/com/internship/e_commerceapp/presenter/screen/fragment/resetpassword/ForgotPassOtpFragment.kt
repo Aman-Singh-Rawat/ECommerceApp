e0b98bb175a47ebd6d132dd7db8e96d6406413f5
Aman Singh <amanrawat6767@gmail.com>
2024-07-30 20:55:38 +0530

    navigating fragment_create_new_pass.xml

--- app/src/main/java/com/internship/e_commerceapp/presenter/screen/fragment/resetpassword/ForgotPassOtpFragment.kt
+++ app/src/main/java/com/internship/e_commerceapp/presenter/screen/fragment/resetpassword/ForgotPassOtpFragment.kt
@@ -5,15 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.internship.e_commerceapp.R
+import com.internship.e_commerceapp.databinding.FragmentForgotPassOtpBinding
 
 class ForgotPassOtpFragment : Fragment() {
-
+    private var _binding: FragmentForgotPassOtpBinding? = null
+    private val binding: FragmentForgotPassOtpBinding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forgot_pass_otp, container, false)
+    ): View {
+        _binding = FragmentForgotPassOtpBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnVerify.setOnClickListener {
+            findNavController().navigate(R.id.createNewPassFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        _binding = null
     }
 }
